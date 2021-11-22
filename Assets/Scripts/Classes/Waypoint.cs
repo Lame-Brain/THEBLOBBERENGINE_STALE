1b1b7491ed422f0c08d4c2cc4094085a5386de8f
The Lame Brain <thelamebrain@gmail.com>
2021-11-22 15:27:07 -0800

    first commit new
    Starting over

--- Assets/Scripts/Classes/Waypoint.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Waypoint : MonoBehaviour
-{
-    public enum Type { fetch, guard, restless}
-    public Type waypoint_Type;
-}
--- Assets/Scripts/Object Scripts/Hello_I_am_a_Ladder.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Hello_I_am_a_Ladder : MonoBehaviour
-{
-    public int targetLevel;
-    public string destination;
-}
