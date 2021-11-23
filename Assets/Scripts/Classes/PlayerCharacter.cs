56b4c0a42083e1d282209f5047b9673afdf73647
The Lame Brain <thelamebrain@gmail.com>
2021-11-22 21:23:25 -0800

    working on Player Character

--- Assets/Scripts/Classes/PlayerCharacter.cs
+++ Assets/Scripts/Classes/PlayerCharacter.cs
@@ -15,9 +15,12 @@ public class PlayerCharacter
     private int Base_Wis, Mod_Wis;
     private int Base_Chrm, Mod_Chrm;
     private int Base_Health, Mod_Health;
-
-    private float hp, maxHp;
-    private float xp, xpNNL;
+    private int hp, maxHp;
+    private int xp, xpNNL;
+    private int attk_base, attk_mod;
+    private int armorClass_base, armorClass_mod;
+    
+    public int initBonus, hp_per_step;
 
     public int Strength() { return Base_Str + Mod_Str; }
     public int Dexterity() { return Base_Dex + Mod_Dex; }
@@ -25,5 +28,13 @@ public class PlayerCharacter
     public int Wisdom() { return Base_Wis + Mod_Wis; }
     public int Charm() { return Base_Chrm + Mod_Chrm; }
     public int Health() { return Base_Health + Mod_Health; }
+    public int HP() { return hp; }
+    public int maxHP() { return maxHp; }
+    public float HpPercent() { return (maxHp - hp) / maxHp; }
+    public int XP() { return xp; }
+    public int XP_NNL() { return xpNNL; }
+    public float XP_Percent() { return (xpNNL - xp) / xpNNL; }
+    public int Attack() { return attk_base + attk_mod; }
+    public int Armor() { return armorClass_base + armorClass_mod; }
         
 }
